cc92f03d3fa84d08bdb6a10abb43c262ecffcb4c
jiangxiaoxin <jiangyanxin1989@gmail.com>
2018-10-10 14:42:58 +0800

    update
    心型线轨迹不应该掺杂scale，在绘制的时候去修改

--- src/others/draw.heart.js
+++ src/others/draw.heart.js
@@ -11,14 +11,14 @@ console.log(rect);
 const size = new Point(stage.clientWidth, stage.clientHeight)
 const center = new Point(size.x / 2, size.y / 2)
 
-const scale = 2 //放大倍数，1倍太小了。放大倍数变大，相应的其实count也要变大才不会那么锯齿
+const scale = 3 //放大倍数，1倍太小了。放大倍数变大，相应的其实count也要变大才不会那么锯齿
 const points = []
 const count = 50
 for (let index = 0; index < count; index++) {
   let angle = index * Math.PI * 2 / count
   let x = 16 * Math.pow(Math.sin(angle), 3)
   let y =  13 * Math.cos(angle) - 5 * Math.cos(2 * angle) - 2 * Math.cos(3 * angle) - Math.cos(4 * angle)
-  let p = new Point(x * scale, y * scale)
+  let p = new Point(x, y)
   points.push(p)
 }
 
@@ -27,9 +27,9 @@ context.fillStyle = "red"
 let p = points[0]
 context.translate(center.x, center.y)
 context.rotate(Math.PI)
-context.moveTo(p.x, p.y)
+context.moveTo(p.x * scale, p.y * scale)
 for (let index = 1; index < points.length; index++) {
   p = points[index]
-  context.lineTo(p.x, p.y)
+  context.lineTo(p.x * scale, p.y * scale)
 }
 context.fill()
