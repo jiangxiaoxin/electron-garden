941020896b50eb907b87f9775f23169c6b4f4e9e
jiangxiaoxin <jiangyanxin1989@gmail.com>
2018-10-15 18:19:31 +0800

    update project

--- dist/renderer.js
+++ dist/renderer.js
@@ -1,9 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-var container = document.querySelector("h2");
-var p = document.createElement("p");
-p.innerHTML = "JYX";
-p.style.margin = "10px auto";
-container.append(p);
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+console.log(electron_1.remote);
 //# sourceMappingURL=renderer.js.map
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,10 +1,2 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-var container = document.querySelector("h2")
-
-var p = document.createElement("p")
-p.innerHTML = "JYX"
-p.style.margin = "10px auto";
-container.append(p)
+import { remote } from "electron";
+console.log(remote);
